7d55e9194db259a68da94a8fb9a71aa311e9d4e8
AlisonD <itsbasictome@gmail.com>
2020-11-14 17:11:49 -0500

    modified mainactivity layout again

--- app/src/main/java/com/example/cps731project/MainActivity.java
+++ app/src/main/java/com/example/cps731project/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        enteredIngredient = findViewById(R.id.txtIngredient);
+        enteredIngredient = findViewById(R.id.addIngredient);
     }
 
 
