c1200044b0d57e632e862162bcb20591889dfe1b
AlisonD <itsbasictome@gmail.com>
2020-11-21 17:24:57 -0500

    created some layouts

--- app/src/main/java/com/example/cps731project/IngredientList.java
+++ app/src/main/java/com/example/cps731project/IngredientList.java
@@ -3,25 +3,17 @@ package com.example.cps731project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
-import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,12 +21,10 @@ import java.util.Map;
 public class IngredientList extends AppCompatActivity {
     public String ingredient;
 
-    private FirebaseFirestore db;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     private static final String TAG = "IngredientList";
     private static final String KEY_TITLE = "name";
 
-    private RecyclerView mFirestoreList;
-    FirestoreRecyclerAdapter adapter;
     private EditText ingred;
     private Button addBtn;
 
@@ -43,9 +33,6 @@ public class IngredientList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredient_list);
 
-        db = FirebaseFirestore.getInstance();
-
-        //Adding ingredients to Firestore
         final String id = UserID.user_id;
         ingred = findViewById(R.id.addIngredient);
         addBtn = findViewById(R.id.btnAdd);
@@ -76,62 +63,8 @@ public class IngredientList extends AppCompatActivity {
             }
         });
 
-        //Displaying ingredients from Firestore
-        mFirestoreList = findViewById(R.id.recyclerView);
-
-        //Query
-        Query query = db.collection("users").document(id).collection("ingredients");
-
-        //RecyclerOptions
-        FirestoreRecyclerOptions<IngredientsModel> options = new FirestoreRecyclerOptions.Builder<IngredientsModel>()
-                .setQuery(query, IngredientsModel.class)
-                .build();
-
-        adapter = new FirestoreRecyclerAdapter<IngredientsModel, IngredientViewHolder>(options) {
-
-            @NonNull
-            @Override
-            public IngredientViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ingredient_list_view, parent, false);
-                return new IngredientViewHolder(view);
-            }
-
-            @Override
-            protected void onBindViewHolder(@NonNull IngredientViewHolder holder, int position, @NonNull IngredientsModel model) {
-                holder.name.setText(model.getName());
-            }
-        };
-
-        mFirestoreList.setHasFixedSize(true);
-        mFirestoreList.setLayoutManager(new LinearLayoutManager(this));
-        mFirestoreList.setAdapter(adapter);
-
     }
-
-    private class IngredientViewHolder extends RecyclerView.ViewHolder {
-
-        private TextView name;
-        public IngredientViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            name = itemView.findViewById(R.id.ingredient_name);
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        adapter.startListening();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        adapter.stopListening();
-    }
-
     public void getItem() {
 
     }
-
 }
--- app/src/main/java/com/example/cps731project/MainActivity.java
+++ app/src/main/java/com/example/cps731project/MainActivity.java
@@ -1,48 +1,79 @@
 package com.example.cps731project;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     private static final String TAG = "MainActivity";
     private static final String KEY_TITLE = "mealType";
-
     private Spinner mealType;
-    private Button goBtn;
-
-    //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView
+                .OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull final MenuItem item) {
+                int id = item.getItemId();
+
+                //noinspection SimplifiableIfStatement
+
+                if (id == R.id.mainActivity) {
+
+                    Intent navHome = new Intent(MainActivity.this, MainActivity.class);
+                    MainActivity.this.startActivity(navHome);
+                    return true;
+                }
+
+                if (id == R.id.viewFavourites) {
+                    Toast.makeText(MainActivity.this, "Favs has been clicked", Toast.LENGTH_LONG).show();
+                    return true;
+                }
+
+                if (id == R.id.viewHistory) {
+                    Toast.makeText(MainActivity.this, "History has been clicked", Toast.LENGTH_LONG).show();
+                    return true;
+                }
+
+                if (id == R.id.viewAccount) {
+                    Intent navLog = new Intent(MainActivity.this, ViewAccount.class);
+                    MainActivity.this.startActivity(navLog);
+                    return true;
+                }
+                return false;
+
+            }
+        });
         mealType = findViewById(R.id.mealType);
 
         final String id = UserID.user_id;
 
-        goBtn = findViewById(R.id.btnGo);
+        Button goBtn = findViewById(R.id.btnGo);
         goBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
